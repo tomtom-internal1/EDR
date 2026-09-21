f990fa28950eeb1ad5af9e3dcc6e7f54b845abf1
tomtom-internal1 <hossamshady24@gmail.com>
2026-09-21 10:52:13 +0300

    evasion1: repair standalone A06 ETW implementation

--- evasion1/projects/06_etw_private_session/main.c
+++ evasion1/projects/06_etw_private_session/main.c
@@ -6,25 +6,34 @@
 #include "poc_common.h"
 #pragma comment(lib, "advapi32.lib")
 
+static void init_props(EVENT_TRACE_PROPERTIES *p, ULONG cap, WCHAR *name)
+{
+    ZeroMemory(p, cap);
+    p->Wnode.BufferSize = cap;
+    p->Wnode.Flags = WNODE_FLAG_TRACED_GUID;
+    p->Wnode.ClientContext = 1;
+    p->LogFileMode = EVENT_TRACE_PRIVATE_LOGGER_MODE;
+    p->MinimumBuffers = 2;
+    p->MaximumBuffers = 8;
+    p->LoggerNameOffset = sizeof(EVENT_TRACE_PROPERTIES);
+    wcscpy_s((WCHAR *)((BYTE *)p + p->LoggerNameOffset), 256, name);
+}
+
 int main(void)
 {
     const char *id = "A06_ETW_PRIVATE_SESSION";
     WCHAR name[] = L"EDDRR_Evasion1_Private";
 
     ULONG cap = sizeof(EVENT_TRACE_PROPERTIES) + 256 * sizeof(WCHAR);
-    EVENT_TRACE_PROPERTIES *props = (EVENT_TRACE_PROPERTIES *)HeapAlloc(
-        GetProcessHeap(), HEAP_ZERO_MEMORY, cap);
+    EVENT_TRACE_PROPERTIES *props =
+        (EVENT_TRACE_PROPERTIES *)HeapAlloc(GetProcessHeap(),
+                                             HEAP_ZERO_MEMORY, cap);
     if (!props) {
         adv_error(id, 1, "ETW_PRIVATE_SESSION_INTEGRITY", "HeapAlloc");
         return 1;
     }
 
-    props->Wnode.BufferSize = cap;
-    props->Wnode.Flags = WNODE_FLAG_TRACED_GUID;
-    props->Wnode.ClientContext = 1;
-    props->LogFileMode = EVENT_TRACE_PRIVATE_LOGGER_MODE;
-    props->LoggerNameOffset = sizeof(EVENT_TRACE_PROPERTIES);
-    wcscpy_s((WCHAR *)((BYTE *)props + props->LoggerNameOffset), 256, name);
+    init_props(props, cap, name);
 
     TRACEHANDLE session = 0;
     ULONG rc = StartTraceW(&session, name, props);
@@ -35,13 +44,17 @@ int main(void)
         return 1;
     }
 
-    char d[512];
+    char d[640];
     snprintf(d, sizeof(d),
              "{\"session_name\":\"EDDRR_Evasion1_Private\","
-             "\"session_handle\":\"0x%llX\",\"mode\":\"private_logger\","
-             "\"owned_by_test_process\":true}",
-             (unsigned long long)session);
-    adv_emit(id, 1, "SessionStarted", "ETW_PRIVATE_SESSION_INTEGRITY", d);
+             "\"session_handle\":\"0x%llX\","
+             "\"mode\":\"private_logger\","
+             "\"minimum_buffers\":%lu,\"maximum_buffers\":%lu}",
+             (unsigned long long)session,
+             (unsigned long)props->MinimumBuffers,
+             (unsigned long)props->MaximumBuffers);
+    adv_emit(id, 1, "SessionStarted",
+             "ETW_PRIVATE_SESSION_INTEGRITY", d);
 
     ZeroMemory(props, cap);
     props->Wnode.BufferSize = cap;
@@ -49,27 +62,38 @@ int main(void)
     wcscpy_s((WCHAR *)((BYTE *)props + props->LoggerNameOffset), 256, name);
 
     rc = ControlTraceW(session, name, props, EVENT_TRACE_CONTROL_QUERY);
+
     snprintf(d, sizeof(d),
-             "{\"query_result\":%lu,\"buffers_written\":%lu,"
-             "\"events_lost\":%lu,\"buffers_lost\":%lu,"
-             "\"log_buffers_lost\":%lu}",
+             "{\"query_result\":%lu,"
+             "\"buffers_written\":%lu,"
+             "\"events_lost\":%lu,"
+             "\"log_buffers_lost\":%lu,"
+             "\"real_time_buffers_lost\":%lu,"
+             "\"number_of_buffers\":%lu}",
              (unsigned long)rc,
              (unsigned long)props->BuffersWritten,
              (unsigned long)props->EventsLost,
-             (unsigned long)props->BuffersLost,
-             (unsigned long)props->LogBuffersLost);
-    adv_emit(id, 2, "SessionQuery", "ETW_PRIVATE_SESSION_INTEGRITY", d);
+             (unsigned long)props->LogBuffersLost,
+             (unsigned long)props->RealTimeBuffersLost,
+             (unsigned long)props->NumberOfBuffers);
+    adv_emit(id, 2, "SessionQuery",
+             "ETW_PRIVATE_SESSION_INTEGRITY", d);
 
-    rc = ControlTraceW(session, name, props, EVENT_TRACE_CONTROL_STOP);
+    ULONG stop_rc = ControlTraceW(session, name, props,
+                                  EVENT_TRACE_CONTROL_STOP);
+
     snprintf(d, sizeof(d),
              "{\"stop_result\":%lu,\"owned_session\":true,"
-             "\"external_session_modified\":false}", (unsigned long)rc);
-    adv_emit(id, 3, "SessionStopped", "ETW_PRIVATE_SESSION_INTEGRITY", d);
+             "\"external_session_modified\":false}",
+             (unsigned long)stop_rc);
+    adv_emit(id, 3, "SessionStopped",
+             "ETW_PRIVATE_SESSION_INTEGRITY", d);
 
-    adv_emit(id, 4, "DetectionOracle", "ETW_PRIVATE_SESSION_INTEGRITY",
+    adv_emit(id, 4, "DetectionOracle",
+             "ETW_PRIVATE_SESSION_INTEGRITY",
              "{\"expected_detection\":\"unexpected_ETW_session_state_change\","
-             "\"baseline\":\"session_name_mode_buffers_lost_events_lost\"}");
+             "\"baseline\":\"session_name_mode_buffer_counts_loss_counters\"}");
 
     HeapFree(GetProcessHeap(), 0, props);
-    return 0;
+    return stop_rc == ERROR_SUCCESS ? 0 : 2;
 }
